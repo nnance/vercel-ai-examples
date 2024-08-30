11df180531bd83df52657cd083f12f42e90b94fd
Nick Nance <nance.nick@gmail.com>
2024-08-29 20:18:36 -0500

    local llm support via ollama

--- longterm-memory/next.config.mjs
+++ longterm-memory/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-    GROQ_API_KEY: "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",
+        PROVIDER: "local",
+        GROQ_API_KEY: "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",
+        GROQ_MODEL: "llama3-groq-70b-8192-tool-use-preview",
+        LOCAL_MODEL: "llama3.1",
     }
 };
 
--- longterm-memory/src/ai/llm.ts
+++ longterm-memory/src/ai/llm.ts
@@ -1,5 +1,6 @@
 import { generateText } from "ai";
 import { createOpenAI as createGroq } from "@ai-sdk/openai";
+import { createOpenAI as createOllama } from "@ai-sdk/openai";
 
 export interface LLMChatMessage {
   text: string;
@@ -12,16 +13,38 @@ export interface LLMChatMessage {
     | "ACTION_ASSIGNER"
     | "CATEGORY_ASSIGNER";
 }
+
+function getProvider() {
+  if (process.env.PROVIDER === "groq") {
+    return createGroq({
+      baseURL: "https://api.groq.com/openai/v1",
+      apiKey: process.env.GROQ_API_KEY,
+    });
+  } else {
+    return createOllama({
+      baseURL: "http://127.0.0.1:11434/v1",
+      apiKey: "ollama",
+    });
+  }
+}
+
+function getModelName() {
+  if (process.env.PROVIDER === "groq") {
+    return process.env.GROQ_MODEL!;
+  } else {
+    return process.env.LOCAL_MODEL!;
+  }
+}
+
 export async function chatCompletion(
   message: LLMChatMessage
 ): Promise<LLMChatMessage> {
-  const groq = createGroq({
-    baseURL: "https://api.groq.com/openai/v1",
-    apiKey: process.env.GROQ_API_KEY,
-  });
+  const provider = getProvider();
+  const modelName = getModelName();
+  const model = provider(modelName);
 
   const { text } = await generateText({
-    model: groq("llama3-groq-70b-8192-tool-use-preview"),
+    model,
     prompt: message.text,
   });
 
